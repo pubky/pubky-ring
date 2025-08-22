e65e31ee51330b89d1083ba2b4c87ef27668f8a3
James Browning <jamesbrowning91@gmail.com>
2025-08-22 13:28:43 +0100

    back to gh hosted with more mem

--- e2e/specs/onboarding.spec.js
+++ e2e/specs/onboarding.spec.js
@@ -11,20 +11,30 @@ const { expect } = require('chai');
 
 describe('Onboarding flow', () => {
 	it('should accept terms and navigate to Home', async () => {
-		// Terms screen
-		const termsScreen = await $('~TermsContinueButton');
-		await termsScreen.waitForDisplayed({ interval: 1000, timeout: 120000 });
+		// // Terms screen
+		// const termsScreen = await $('~TermsContinueButton');
+		// await termsScreen.waitForDisplayed({ interval: 1000 });
 
-		const termsRow = await $('~TermsAgreeRow');
-		await termsRow.click();
+		// const termsRow = await $('~TermsAgreeRow');
+		// await termsRow.click();
 
-		const privacyRow = await $('~PrivacyAgreeRow');
-		await privacyRow.click();
+		// const privacyRow = await $('~PrivacyAgreeRow');
+		// await privacyRow.click();
 
-		const continueBtn = await $('~TermsContinueButton');
-		await continueBtn.click();
+		// const continueBtn = await $('~TermsContinueButton');
+		// await continueBtn.click();
 
-		const onboardingGetStartedButton = await $('~OnboardingGetStartedButton');
-		expect(await onboardingGetStartedButton.isDisplayed()).to.equal(true);
+		// const onboardingGetStartedButton = await $('~OnboardingGetStartedButton');
+		// expect(await onboardingGetStartedButton.isDisplayed()).to.equal(true);
+    
+    // const tb1 = await $('~testId:Check1');
+    // const tb2 = await $('~testId:Check2');
+    // const getStartedButton = await $('~testId:GetStarted');
+    const continueBtn = await $('~testId:Continue');
+    // await tb1.waitForDisplayed({ interval: 1000 });
+    // await tb1.click();
+    // await tb2.click();
+    // await continueBtn.click();
+    expect(await continueBtn.isDisplayed()).to.equal(true);
 	});
 });
--- e2e/wdio.android.conf.js
+++ e2e/wdio.android.conf.js
@@ -30,15 +30,15 @@ exports.config = {
 			...(resolvedAppPath
 				? {
 					'appium:app': resolvedAppPath,
-					'appium:appWaitPackage': process.env.APP_PACKAGE || 'to.pubky.ring',
-					'appium:appActivity': process.env.APP_ACTIVITY || 'to.pubkyring.MainActivity',
-					'appium:appWaitActivity': process.env.APP_WAIT_ACTIVITY || 'to.pubkyring.MainActivity',
-					'appium:forceAppLaunch': false
+					// 'appium:appWaitPackage': process.env.APP_PACKAGE || 'com.bitkit',
+					// 'appium:appActivity': process.env.APP_ACTIVITY || 'com.bitkit.MainActivity',
+					// 'appium:appWaitActivity': process.env.APP_WAIT_ACTIVITY || 'com.bitkit.MainActivity',
+					// 'appium:forceAppLaunch': false
 				}
 				: {
-					'appium:appPackage': process.env.APP_PACKAGE || 'to.pubky.ring',
-					'appium:appActivity': process.env.APP_ACTIVITY || 'to.pubkyring.MainActivity',
-					'appium:appWaitActivity': process.env.APP_WAIT_ACTIVITY || 'to.pubkyring.MainActivity',
+					'appium:appPackage': process.env.APP_PACKAGE || 'com.bitkit',
+					'appium:appActivity': process.env.APP_ACTIVITY || 'com.bitkit.MainActivity',
+					'appium:appWaitActivity': process.env.APP_WAIT_ACTIVITY || 'com.bitkit.MainActivity',
 					'appium:forceAppLaunch': false
 				})
 		}

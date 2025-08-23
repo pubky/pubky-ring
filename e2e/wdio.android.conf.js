cf1d33933d975f829a64c79847b7d2422e96a207
jazkamer <v.rodionov@protonmail.com>
2025-08-23 22:00:15 +0300

    test: deploy_research: fix test

--- e2e/wdio.android.conf.js
+++ e2e/wdio.android.conf.js
@@ -17,13 +17,15 @@ exports.config = {
 			'appium:deviceName': process.env.ANDROID_DEVICE_NAME || 'Android Emulator',
 			'appium:avd': avdName,
 			'appium:autoGrantPermissions': true,
+			'appium:allowTestPackages': true,
+			'appium:enforceAppInstall': true,
 			...(resolvedAppPath
 				? {
 					'appium:app': resolvedAppPath,
 					'appium:appWaitPackage': process.env.APP_PACKAGE || 'to.pubky.ring',
 					'appium:appActivity': process.env.APP_ACTIVITY || 'to.pubkyring.MainActivity',
 					'appium:appWaitActivity': process.env.APP_WAIT_ACTIVITY || 'to.pubkyring.MainActivity',
-					'appium:forceAppLaunch': true
+					'appium:forceAppLaunch': false
 				}
 				: {
 					'appium:appPackage': process.env.APP_PACKAGE || 'to.pubky.ring',

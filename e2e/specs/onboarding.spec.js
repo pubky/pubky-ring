fd97050d25da0d7659eecb69e3f6e5d1323bc708
jazkamer <v.rodionov@protonmail.com>
2025-08-27 20:11:20 +0300

    test: deploy_research: test debug app build

--- e2e/specs/onboarding.spec.js
+++ e2e/specs/onboarding.spec.js
@@ -10,10 +10,9 @@ export const sleep = (ms) => browser.pause(ms);
 
 describe('Onboarding flow', () => {
     it('should accept terms and navigate to Home', async () => {
-		sleep(10000);
+		sleep(20000);
         const t = await elementByText("I declare that I have read and accept the terms of use.").click();
         const p = await elementByText("I declare that I have read and accept the privacy policy.").click();
         const r = await elementByText("Continue").click();
-		sleep(1000000);
     });
 });

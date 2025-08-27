794dc70bd7a49ba70bb2f20af42deee4727b0808
jazkamer <v.rodionov@protonmail.com>
2025-08-27 22:30:28 +0300

    test: deploy_research: test debug app build

--- e2e/specs/onboarding.spec.js
+++ e2e/specs/onboarding.spec.js
@@ -1,6 +1,16 @@
+//const { expect } = require('chai');
+
 export function elementByText(text) {
     if (driver.isAndroid) {
         return $(`android=new UiSelector().textContains("${text}")`);
+    } else {
+        return $(`~test`);
+    }
+}
+
+export function elementByID(id) {
+    if (driver.isAndroid) {
+        return $(`android=new UiSelector().resourceId("${id}")`);
     } else {
         return $(`~test`);
     }
@@ -10,9 +20,26 @@ export const sleep = (ms) => browser.pause(ms);
 
 describe('Onboarding flow', () => {
     it('should accept terms and navigate to Home', async () => {
-		sleep(20000);
-        const t = await elementByText("I declare that I have read and accept the terms of use.").click();
-        const p = await elementByText("I declare that I have read and accept the privacy policy.").click();
-        const r = await elementByText("Continue").click();
+//		sleep(4000);
+        //const t = await elementByText("I declare that I have read and accept the terms of use.").click();
+        //const p = await elementByText("I declare that I have read and accept the privacy policy.").click();
+        //const t = await elementByText("I declare that I have read and accept the terms of use.");
+        //const p = await $('~TermsContinueButton');
+        //console.log(p)
+		const termsScreen = await $('~TermsContinueButton');
+		await termsScreen.waitForDisplayed({ interval: 5000, timeout: 60_000 });
+		console.log(termsScreen)
+        //console.log(elementsList)
+        //const r = await elementByText("Continue").click();
+		const termsRow = await $('~TermsAgreeRow');
+		await termsRow.click();
+
+		const privacyRow = await $('~PrivacyAgreeRow');
+		await privacyRow.click();
+
+		const continueBtn = await $('~TermsContinueButton');
+		await continueBtn.click();
+
+		const onboardingGetStartedButton = await $('~OnboardingGetStartedButton');
     });
 });

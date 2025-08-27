6822b6ad7bf719d780b82c5719e477d7e9814d68
coreyphillips <corey.lyle.phillips@proton.me>
2025-08-27 12:43:26 -0400

    feat: implement scan to import
    - Implements the scan to import feature when tapping Add Pubky->Import pubky
    - Updates .lock files
    - Updates styling in AddPubky.tsx component
    - Adds animation constants to constants.ts

--- src/hooks/usePubkyManagement.ts
+++ src/hooks/usePubkyManagement.ts
@@ -7,7 +7,7 @@ import {
 	createNewPubky,
 	importPubky as importPubkyUtil,
 } from '../utils/pubky';
-import { showToast } from '../utils/helpers';
+import { isSecretKeyImport, showToast } from '../utils/helpers';
 import { importFile } from '../utils/rnfs';
 import { showEditPubkySheet,
 	showImportSuccessSheet,
@@ -44,55 +44,82 @@ export const usePubkyManagement = (): {
 	}, [dispatch]);
 
 	const importPubky = useCallback(
-		async (mnemonic = ''): Promise<Result<string>> => {
+		async (data = ''): Promise<Result<string>> => {
 			const currentPubkys = getPubkyKeys(getStore());
-			if (mnemonic) {
-				const secretKeyRes: Result<IGenerateSecretKey> =
-					await mnemonicPhraseToKeypair(mnemonic);
-				if (secretKeyRes.isErr()) {
-					const msg = secretKeyRes.error.message;
-					showToast({
-						type: 'error',
-						title: 'Error',
-						description: msg,
-					});
-					return err(msg);
+			if (data) {
+				// Check if it's a secret key/mnemonic phrase
+				const res = await isSecretKeyImport(data);
+				if (res.isErr()) {
+					return err(res.error.message);
+				}
+				if (!res.value.isSecretKey) {
+					return err('Not a valid secret key or recovery phrase.');
 				}
 
-				const secretKey: string = secretKeyRes.value.secret_key;
-				const pubky = await importPubkyUtil({ secretKey, dispatch, mnemonic });
-				if (pubky.isErr()) {
-					const msg = pubky.error.message;
-					showToast({
-						type: 'error',
-						title: 'Error',
-						description: msg,
-					});
-					return err(msg);
+				let secretKey = '';
+				let mnemonic = '';
+				switch (res.value.backupPreference) {
+					case EBackupPreference.encryptedFile:
+						secretKey = data;
+						break;
+					case EBackupPreference.recoveryPhrase:
+						mnemonic = data;
+						break;
 				}
-				const isNewPubky = !currentPubkys.includes(pubky.value);
-				await SheetManager.hide('add-pubky');
-				setTimeout(() => {
-					showImportSuccessSheet({
-						pubky: pubky.value,
-						isNewPubky,
-						onContinue: () => {
-							setTimeout(() => {
-								showEditPubkySheet({
-									title: 'Setup',
-									pubky: pubky.value,
-								});
-							}, 200);
-						}
+
+				if (mnemonic) {
+					const secretKeyRes: Result<IGenerateSecretKey> =
+            await mnemonicPhraseToKeypair(mnemonic);
+					if (secretKeyRes.isErr()) {
+						const msg = secretKeyRes.error.message;
+						showToast({
+							type: 'error',
+							title: 'Error',
+							description: msg,
+						});
+						return err(msg);
+					}
+					secretKey = secretKeyRes.value.secret_key;
+				}
+				if (mnemonic || secretKey) {
+					const pubky = await importPubkyUtil({
+						secretKey,
+						dispatch,
+						mnemonic,
 					});
-				}, 200);
-				return ok('Successfully created pubky.');
+					if (pubky.isErr()) {
+						const msg = pubky.error.message;
+						showToast({
+							type: 'error',
+							title: 'Error',
+							description: msg,
+						});
+						return err(msg);
+					}
+					const isNewPubky = !currentPubkys.includes(pubky.value);
+					await SheetManager.hide('add-pubky');
+					setTimeout(() => {
+						showImportSuccessSheet({
+							pubky: pubky.value,
+							isNewPubky,
+							onContinue: () => {
+								setTimeout(() => {
+									showEditPubkySheet({
+										title: 'Setup',
+										pubky: pubky.value,
+									});
+								}, 200);
+							},
+						});
+					}, 200);
+					return ok('Successfully created pubky.');
+				}
 			}
 
-			const res = await importFile(dispatch);
-			if (res.isErr()) {
-				const msg = res.error?.message ?? 'Unable to import file.';
-				if (res.error?.message && res.error.message !== 'OPERATION_CANCELLED') {
+			const importFileRes = await importFile(dispatch);
+			if (importFileRes.isErr()) {
+				const msg = importFileRes.error?.message ?? 'Unable to import file.';
+				if (importFileRes.error?.message && importFileRes.error.message !== 'OPERATION_CANCELED') {
 					showToast({
 						type: 'error',
 						title: 'Error',
@@ -108,16 +135,16 @@ export const usePubkyManagement = (): {
 				return err(msg);
 			}
 
-			const isNewPubky = !currentPubkys.includes(res.value);
+			const isNewPubky = !currentPubkys.includes(importFileRes.value);
 			setTimeout(() => {
 				showImportSuccessSheet({
-					pubky: res.value,
+					pubky: importFileRes.value,
 					isNewPubky,
 					onContinue: () => {
 						setTimeout(() => {
 							showEditPubkySheet({
 								title: 'Setup',
-								pubky: res.value,
+								pubky: importFileRes.value,
 							});
 						}, 200);
 					}

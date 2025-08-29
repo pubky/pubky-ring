8ea194aea985634260d156366b9b7763121b8352
jazkamer <v.rodionov@protonmail.com>
2025-08-29 11:16:43 +0300

    test: deploy_research: test debug app build

--- e2e/specs/onboarding.spec.js
+++ e2e/specs/onboarding.spec.js
@@ -20,24 +20,24 @@ export const sleep = (ms) => browser.pause(ms);
 
 describe('Onboarding flow', () => {
     it('should accept terms and navigate to Home', async () => {
-        //const t = await elementByText("I declare that I have read and accept the terms of use.").click();
-        //const p = await elementByText("I declare that I have read and accept the privacy policy.").click();
+        const t = await elementByText("I declare that I have read and accept the terms of use.").click();
+        const p = await elementByText("I declare that I have read and accept the privacy policy.").click();
         //const t = await elementByText("I declare that I have read and accept the terms of use.");
         //const p = await $('~TermsContinueButton');
         //console.log(p)
-		const termsScreen = await $('~TermsContinueButton');
+//		const termsScreen = await $('~TermsContinueButton');
 		//await termsScreen.waitForDisplayed({ interval: 5000, timeout: 60000 });
 //		console.log(termsScreen)
         //console.log(elementsList)
         //const r = await elementByText("Continue").click();
-		const termsRow = await $('~TermsAgreeRow');
-		await termsRow.click();
+//		const termsRow = await $('~TermsAgreeRow');
+//		await termsRow.click();
 
-		const privacyRow = await $('~PrivacyAgreeRow');
-		await privacyRow.click();
-
-		const continueBtn = await $('~TermsContinueButton');
-		await continueBtn.click();
+//		const privacyRow = await $('~PrivacyAgreeRow');
+//		await privacyRow.click();
+//
+//		const continueBtn = await $('~TermsContinueButton');
+//		await continueBtn.click();
 
 		//const onboardingGetStartedButton = await $('~OnboardingGetStartedButton');
     });

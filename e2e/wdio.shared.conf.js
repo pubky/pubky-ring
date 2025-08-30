21825331461f1bc2490cfe8a124b15ddf9f910c7
jazkamer <v.rodionov@protonmail.com>
2025-08-30 14:43:47 +0300

    test: actions_fix: trying https://github.com/appium/appium/issues/21095

--- e2e/wdio.shared.conf.js
+++ e2e/wdio.shared.conf.js
@@ -11,6 +11,7 @@ exports.config = {
 	waitforTimeout: 30_000,
 	connectionRetryTimeout: 300_000,
 	connectionRetryCount: 2,
+	xvfbAutoInstall: true,
 	framework: 'mocha',
 	reporters: ['spec'],
 	mochaOpts: {

12f03f4f6f388f073b1f8b00bda17faefb522f05
James Browning <jamesbrowning91@gmail.com>
2025-09-02 17:28:33 +0100

    tests: create pubky test with invalid invite code (prod)

--- e2e/specs/onboarding.spec.js
+++ e2e/specs/onboarding.spec.js
@@ -1,37 +1,16 @@
-const { expect } = require('chai');
-const { elementById, waitForDisplayed } = require('../helpers/actions');
+const { completeOnboardingFlow } = require('../helpers/actions');
 
 /**
  * Basic onboarding flow:
  * 1) Accept both checkboxes on Terms screen, tap Continue.
- * 2) On Onboarding screen, check Get Started button is displayed.
+ * 2) On Onboarding screen, check Get Started button is displayed, tap it.
+ * 3) On Home screen, check Add Pubky button is displayed.
  */
-
-// Note: This test will be redone when we have the new onboarding flow and more tests will be added.
-// We will also use PageObjectModel pattern for the tests, not using selectors and Appium api directly.
 
 describe('Onboarding flow', () => {
 
-	it('should accept terms and navigate to Home', async () => {
-		const termsScreen = await elementById('TermsOfUseTitle');
-		await waitForDisplayed(termsScreen, 60_000);
-		expect(await termsScreen.isDisplayed()).to.be.true;
-		expect(await termsScreen.getText()).to.equal('Terms of Use.');
-
-		const termsRow = await elementById('TermsAgreeRow');
-		await termsRow.click();
-
-		const privacyRow = await elementById('PrivacyAgreeRow');
-		await privacyRow.click();
-
-		const continueBtn = await elementById('TermsContinueButton');
-		await continueBtn.click();
-
-		const onboardingGetStartedButton = await elementById('OnboardingGetStartedButton');
-		await onboardingGetStartedButton.click();
-
-		const addPubkyButton = await elementById('EmptyStateAddPubkyButton');
-		await waitForDisplayed(addPubkyButton);
-		expect(await addPubkyButton.isDisplayed()).to.be.true;
+	it('can accept terms and navigate to Home', async () => {
+		await completeOnboardingFlow();
+		// TODO: add check for pubky placeholder
 	});
 });

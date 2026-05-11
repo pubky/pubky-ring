1169e109d4960dd260495b3356150d7da08676d0
Philipp Walter <philippwalter@proton.me>
2026-05-11 20:51:04 +0200

    chore: upgrade prettier, add format scripts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  trailingComma: 'all',
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	printWidth: 110,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 2,
+	useTabs: true,
 };

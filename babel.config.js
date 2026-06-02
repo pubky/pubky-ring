2073e326b172234a868b3ff66793b39cf8c02435
coreyphillips <corey.lyle.phillips@proton.me>
2026-06-02 10:29:44 -0400

    perf: apply React Native performance best practices
    - Enable React Compiler (babel-plugin-react-compiler) for automatic memoization
    - Stabilize SelectPubky FlashList renderItem/keyExtractor and memoize row
     - Disable Hermes bytecode bundle compression on Android for mmap (faster TTI)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
 	presets: ['module:@react-native/babel-preset'],
 	plugins: [
+		// React Compiler must run first so it can analyze components before
+		// other transforms. react-native-worklets/plugin must remain last.
+		'babel-plugin-react-compiler',
 		'react-native-worklets/plugin',
 	],
 };
